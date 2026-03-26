96e6cb7dd1b222919f2d1227cac568c195b4fad2
Claire Code <desirervainceur@gmail.com>
2026-03-26 21:43:26 +0000

    again

--- dist-electron/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("electronAPI", {
-  getApiKey: () => electron.ipcRenderer.invoke("get-api-key"),
-  setApiKey: (key) => electron.ipcRenderer.invoke("set-api-key", key),
-  getBackendPort: () => electron.ipcRenderer.invoke("get-backend-port"),
-  getTheme: () => electron.ipcRenderer.invoke("get-theme"),
-  setTheme: (theme) => electron.ipcRenderer.invoke("set-theme", theme)
-});
